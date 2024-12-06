adbc5266b39ce67cd675e9a19a657b920951207d
Deiver Vasquez <estudiandovazmore@gmail.com>
2024-12-05 21:01:54 -0500

    minor changes

--- app/modules/user_recognition/models/ingressRecord.py
+++ app/modules/user_recognition/models/ingressRecord.py
@@ -6,10 +6,10 @@ class IngressRecord(db.Model):
     __tablename__ = 'ingress_records'
 
     id = db.Column(db.Integer, primary_key=True)
-    time_stamp = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=False)
-    suggestions_comments = db.Column(db.String(50), unique=True, nullable=False)
-    proteccion_notice = db.Column(db.String(20), nullable=False)
-    services_library = db.Column(db.String(20), nullable=False)
+    time_stamp = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    suggestions_comments = db.Column(db.String(50), nullable=True)
+    protection_notice = db.Column(db.String(20), nullable=True)
+    services_library = db.Column(db.String(20), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     user = db.relationship('User', back_populates='ingress_records')
 
